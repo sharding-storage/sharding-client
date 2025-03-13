a77ed31b654624db6c741d54b590f43287ccbe73
Mikhail Oskilko <311707@niuitmo.ru>
2025-03-13 22:54:32 +0300

    test starter

--- src/main/java/ru/itmo/vk/ProgramRunner.java
+++ src/main/java/ru/itmo/vk/ProgramRunner.java
@@ -2,15 +2,19 @@ package ru.itmo.vk;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import ru.itmo.vk.client.Client;
 import ru.itmo.vk.client.MasterNode;
 import ru.itmo.vk.hash.MD5HashFunction;
 
+@Slf4j
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProgramRunner {
     public static void start() {
         MasterNode masterNode = new MasterNode("address");
         Client client = new Client(new MD5HashFunction(), masterNode);
+
+        System.out.println("Program started");
     }
 }
 
