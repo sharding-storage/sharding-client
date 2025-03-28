e7a0fc36bf5a18d90d71e96dce553349da474483
Mikhail Oskilko <311707@niuitmo.ru>
2025-03-28 14:11:19 +0300

    change hash to MD5

--- src/main/java/ru/itmo/vk/hash/MD5HashFunction.java
+++ src/main/java/ru/itmo/vk/hash/MD5HashFunction.java
@@ -10,15 +10,15 @@ public class MD5HashFunction implements HashFunction {
     @Override
     public int hash(String key) {
         //return Hashing.murmur3_32_fixed().hashUnencodedChars(key).asInt();
-        return Hashing.sha256()
-            .hashString(key, StandardCharsets.UTF_8)
-            .asInt();
-//        try {
-//            //MessageDigest md = MessageDigest.getInstance("MD5");
-//            //byte[] digest = md.digest(key.getBytes());
-//            //return new BigInteger(1, digest).intValue();
-//        } catch (java.security.NoSuchAlgorithmException e) {
-//            throw new RuntimeException("MD5 algorithm not found", e);
-//        }
+//        return Hashing.sha256()
+//            .hashString(key, StandardCharsets.UTF_8)
+//            .asInt();
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] digest = md.digest(key.getBytes());
+            return new BigInteger(1, digest).intValue();
+        } catch (java.security.NoSuchAlgorithmException e) {
+            throw new RuntimeException("MD5 algorithm not found", e);
+        }
     }
 }
